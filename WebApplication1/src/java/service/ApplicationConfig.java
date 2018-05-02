39b37197cbb52da49530f1f4abe7108ffe06de5b
Kaisa Löfman <kaisalo@metropolia.fi>
2018-05-02 10:45:33 +0300

    vds

--- WebApplication1/src/java/service/ApplicationConfig.java
+++ WebApplication1/src/java/service/ApplicationConfig.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Application;
  *
  * @author kaisa
  */
-@javax.ws.rs.ApplicationPath("webresources")
+@javax.ws.rs.ApplicationPath("wr")
 public class ApplicationConfig extends Application {
 
     @Override
