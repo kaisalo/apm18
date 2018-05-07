9ab6b50a4a611bf52f8ccfaaa89302dbd2603795
Kaisa Löfman <kaisalo@metropolia.fi>
2018-05-07 11:15:58 +0300

    tasks.js unfinished

--- WebApplication1/web/tasks.js
+++ WebApplication1/web/tasks.js
@@ -6,34 +6,80 @@ function addButton() {
 function cancelButton() {
     document.getElementById("hidden-add-form").style.visibility = "hidden";
     document.getElementById("my-form").reset();
-
     /* document.getElementById('urgent-btn').checked = false; */
 }
 ; // The X sign button that cancel add task in the form
 
 //IN PROGRESS
 let apiUrl = 'wr/';
-
-fetch(apiUrl + 'task')
-        .then(re => res.json())
+fetch(apiUrl + 'client')
+        .then(resc => resc.json())
         .then(function (json) {
-            tasksFetch(json);
+            clientsFetch(json);
         })
         .catch(err => console.log(err));
 
-const taskFetch = tasks => {
+
+const clientsFetch = clients => {
     let htmlString = '';
     let clientName = '';
-
-    for (let task of tasks) {
-        fetch(apiUrl + 'client/' + task.clientID)
-                .then(res => res.json())
+    let locName = '';
+    let taskNum = 0;
+    let button = false;
+    
+    for (let client of clients) {
+        console.log(client);
+        fetch(apiUrl + 'client/' + client.clientid)
+                .then(resc => resc.json())
                 .then(json => {
                     clientName = json.clientname;
-                    htmlString += `<div class="title">${task.clientname}></div>
-<ul><li class ="checked">
-<span>${task.}`;
+                    htmlString += `<div class ="title">${clientName}</div>
+                        <ul>
+                            <li class="checked">`;
+                    let locationFetch = locs => {
+                        
+
+                        for (let loc of locs) {
+                            fetch(apiUrl + 'location/' + loc.locationID)
+                                    .then(resl => resl.json())
+                                    .then(json => {
+                                        locName = json.locname;
+                                        
+                                        htmlString += `<span>${locName}</span>
+                                        <div class="info">`;
+                                        
+                                        let taskFetch = tasks => {                                            
+                                            
+                                            for (let task of tasks){
+                                                fetch(apiUrl + 'task/'+taskid)
+                                                        .then(rest => rest.json())
+                                                        .then(json =>{
+                                                            taskNum++;
+                                                            button = true;
+                                                            
+                                                        })
+                                                .catch(err => console.log(err));
+                                        
+                                            }
+                                            if(button){
+                                                htmlString += `<div class = "button green">Finished</div>`;
+                                            }else{
+                                                htmlString += `<div class = "button">Tasks: ${taskNum}</div>`;
+                                            }
+                                        };
+                                        
+                                    })
+                                    .catch(err => console.log(err));
+
+                        }
+                    };
+                    htmlString += `</div>
+                    </li>
+                    </ul>`;
+                    document.querySelector('#locstest').innerHTML = htmlString;
+
                 })
+                .catch(err => console.log(err));
     }
 
 };
