ea7ec25fc50564ed83b46b9ddc6815558c88e9aa
Kaisa Löfman <kaisalo@metropolia.fi>
2018-05-07 17:42:31 +0300

    task.js fetch

--- WebApplication1/web/tasks.js
+++ WebApplication1/web/tasks.js
@@ -10,23 +10,23 @@ tasksBtn.classList.add("active");
 logsView.classList.add("hidden");
 logsContent.classList.add("hidden");
 
-tasksBtn.addEventListener("click", function(event) {
+tasksBtn.addEventListener("click", function (event) {
     tasksBtn.classList.add("active");
     logsBtn.classList.remove("active");
-    
+
     logsContent.classList.add("hidden");
     logsView.classList.add("hidden");
-    
-    tasksView.classList.remove("hidden");  
+
+    tasksView.classList.remove("hidden");
     tasksContent.classList.remove("hidden");
 });
-logsBtn.addEventListener("click", function(event) {
+logsBtn.addEventListener("click", function (event) {
     logsBtn.classList.add("active");
     tasksBtn.classList.remove("active");
-    
+
     logsView.classList.remove("hidden");
     logsContent.classList.remove("hidden");
-    
+
     tasksView.classList.add("hidden");
     tasksContent.classList.add("hidden");
 });
@@ -60,7 +60,7 @@ const clientsFetch = clients => {
     let locName = '';
     let taskNum = 0;
     let button = false;
-    
+
     for (let client of clients) {
         console.log(client);
         fetch(apiUrl + 'client/' + client.clientid)
@@ -71,37 +71,36 @@ const clientsFetch = clients => {
                         <ul>
                             <li class="checked">`;
                     let locationFetch = locs => {
-                        
+
 
                         for (let loc of locs) {
                             fetch(apiUrl + 'location/' + loc.locationID)
                                     .then(resl => resl.json())
                                     .then(json => {
                                         locName = json.locname;
-                                        
                                         htmlString += `<span>${locName}</span>
                                         <div class="info">`;
-                                        
-                                        let taskFetch = tasks => {                                            
-                                            
-                                            for (let task of tasks){
-                                                fetch(apiUrl + 'task/'+taskid)
+
+                                        let taskFetch = tasks => {
+
+                                            for (let task of tasks) {
+                                                fetch(apiUrl + 'task/' + taskid)
                                                         .then(rest => rest.json())
-                                                        .then(json =>{
+                                                        .then(json => {
                                                             taskNum++;
                                                             button = true;
-                                                            
+
                                                         })
-                                                .catch(err => console.log(err));
-                                        
+                                                        .catch(err => console.log(err));
+
                                             }
-                                            if(button){
+                                            if (button) {
                                                 htmlString += `<div class = "button green">Finished</div>`;
-                                            }else{
+                                            } else {
                                                 htmlString += `<div class = "button">Tasks: ${taskNum}</div>`;
                                             }
                                         };
-                                        
+
                                     })
                                     .catch(err => console.log(err));
 
@@ -117,8 +116,8 @@ const clientsFetch = clients => {
     }
 
 };
-  /* document.getElementById('urgent-btn').checked = false; */
-  
+/* document.getElementById('urgent-btn').checked = false; */
+
 
 
 // The X sign button that cancel add task in the form
